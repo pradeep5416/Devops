8c66a46717c1a592a7b7d48b6f482830928cd573
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 10:39:02 +0530

    20761A5417

--- Area/src/Area2.java
+++ Area/src/Area2.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Area2 {
 	public void cuboid(float l,float b,float h) {
 		
+		System.out.println(l*b*h);
+		
 	}
     public static void main(String args[]) {
     	Area2 a=new Area2();
