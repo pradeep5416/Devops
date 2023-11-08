9c7c084ec4d10ad1f32be4d2aacf9ffb2edbc668
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-11-08 10:16:56 +0530

    code Build

--- Area/src/Area.java
+++ Area/src/Area.java
@@ -2,6 +2,10 @@ import java.util.*;
 public class Area {
 	public void circle(float r) {
 		
+		double res;
+		res=3.14*r*r;
+		System.out.println(res);
+		
 	}
 	public static void main(String args[]) {
 		Area a=new Area();
